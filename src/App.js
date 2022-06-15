baa9cb92ad7de493021bedb01c851b7bfc6b0d66
Brendon Rodrigues <rodriguesbrendon861@gmail.com>
2022-06-15 09:51:16 -0300

    primeiro comentário

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react"
+
+export default function App (){
+  const [input, setInput] = useState()
+  const [name, setName] = useState()
+  const nomeDoClient = ()=>{
+    setName(input)
+  }
+  return (
+    <form onSubmit={(e)=>{
+      e.preventDefault()
+      setName(input)
+    }}>
+    <input value={input} onChange={(e)=>{
+      setInput(e.target.value)
+    }}/>
+    
+    <h1> Obrigado por comprar {name} </h1>
+    </form> 
+  )
+}
